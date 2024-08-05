3719d0017817141b8b0a5015ea225effd698c8cb
Jon Zimbel <jzimbel@mbta.com>
2024-08-05 14:06:49 -0400

    Formatting

--- test/transit_data/glides_report/util_test.exs
+++ test/transit_data/glides_report/util_test.exs
@@ -1,23 +1,23 @@
 defmodule TransitData.GlidesReport.UtilTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    alias TransitData.GlidesReport.Util
+  alias TransitData.GlidesReport.Util
 
-    describe "stream_values/1" do
-      setup do
-        table = :ets.new(TransitData.GlidesReport.UtilTest.Table)
-        on_exit(fn -> :ets.delete(table) end)
+  describe "stream_values/1" do
+    setup do
+      table = :ets.new(TransitData.GlidesReport.UtilTest.Table)
+      on_exit(fn -> :ets.delete(table) end)
 
-        %{table: table}
-      end
-
-      test "streams values from a table", %{table: table} do
-        objects = [a: 1, b: 2, c: 3, d: 4]
-        :ets.insert(table, objects)
-
-        from_stream = Util.stream_values(table)
-
-        assert Enum.sort(objects) == Enum.sort(from_stream)
-      end
+      %{table: table}
     end
+
+    test "streams values from a table", %{table: table} do
+      objects = [a: 1, b: 2, c: 3, d: 4]
+      :ets.insert(table, objects)
+
+      from_stream = Util.stream_values(table)
+
+      assert Enum.sort(objects) == Enum.sort(from_stream)
+    end
+  end
 end
